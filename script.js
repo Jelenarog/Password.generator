14dbcc86bdf7f5cea972668162167cf3027ea5e3
Jelenarog <jelena.rogic90@gmail.com>
2022-10-31 21:56:51 -0600

    updating java

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ function generatePassword() {
   var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-  ///var checked = null; // try if condition with null?
   var userCharacterSelection = [];//var to store confirm characters
   //confirm what characters do you want to use, for each option please confirm
   var characterName = [" numbers", " special characters", " uppercase letters", " lowercase letters"]
@@ -20,7 +19,7 @@ function generatePassword() {
     for (var i = 0; i < characters.length; i++) {
       var selectCharacter = window.confirm("Would you like to use" + characterName[i] + "?");
       if (selectCharacter) {// what character user wants to use
-        console.log(characters[i]) /////******************** */
+        console.log(characters[i]) 
         userCharacterSelection = userCharacterSelection.concat(characters[i]);//reassign +answer in var userCharacterSelection we will use as an array when generating password
 
       }
@@ -28,29 +27,22 @@ function generatePassword() {
   };
 
   //password length prompt how  many characters to use, alert if <8 or >128 otherwise call function what characters to use
-  var passwordLength = window.prompt("How many characters would you like your password to contain? Choose a length of at least 8 characters and no more than 128 characters",);
-  var numberOfCharacters = function () {
-    if (passwordLength < 8) {
-      window.alert("Please choose number between 8 nad 128.")
-    }
-    else if (passwordLength > 128) {
-      window.alert("Please choose number between 8 nad 128.")
-    }
-    else {
+  var passwordLength = window.prompt("How many characters would you like your password to contain? Choose a length of at least 8 characters and no more than 128 characters");
+
+  while (passwordLength < 8 || passwordLength > 135 || isNaN(passwordLength)){
+      window.alert("Please choose a number between 8 nad 128.");
+      passwordLength = window.prompt("How many characters would you like your password to contain? Choose a length of at least 8 characters and no more than 128 characters");
+      };
+    if (passwordLength >= 8 && passwordLength <= 128) {
       chooseCharacter();
-      console.log(userCharacterSelection);
-    }
-  };
-  numberOfCharacters();
-
-  //random index function to select random index from list of characters user wants to use
-  var randomIndex = function () {
-    for (i = 0; i < passwordLength; i++) {
-      randomIn[i] = Math.floor(Math.random() * userCharacterSelection.length);
-    }
-  }
-  randomIndex();
-
+      var randomIndex = function () {  //random index function to select random index from list of characters user wants to use
+        for (i = 0; i < passwordLength; i++) {
+          randomIn[i] = Math.floor(Math.random() * userCharacterSelection.length);
+        }
+      }
+      randomIndex();
+    };
+ 
  //store all random Characters in variable indexCombiner
   for (i = 0; i < randomIn.length; i++) {
     indexCombiner[i] = userCharacterSelection[randomIn[i]];
